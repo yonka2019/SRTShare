3019c343d9d7f240fa29421c05712746d27bb3d5
Yonka <yonka2003@gmail.com>
2022-11-10 17:51:31 +0200

    added example to usage

--- PcapFunc/ProtocolManager.cs
+++ PcapFunc/ProtocolManager.cs
@@ -5,6 +5,14 @@ namespace SRTManager
 {
     public class ProtocolManager
     {
+        /* Usage Example:
+            EthernetLayer ethernetLayer = PacketManager.BuildEthernetLayer();
+            IpV4Layer ipV4Layer = PacketManager.BuildIpv4Layer();
+            UdpLayer udpLayer = PacketManager.BuildUdpLayer(PacketManager.SERVER_PORT, 123);
+            var a = new ProtocolManager(ethernetLayer, ipV4Layer, udpLayer);
+            Packet doneToSend = a.HandshakeRequest();
+         */
+
         private ILayer[] workingLayers; // layers we working with, we should add last packet data (SRT)
         /*
          * EthernetLayer - Exist
