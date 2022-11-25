ff64ba0da5e33aa1d6e070ba9ee881b16032b81b
EyalSol <eyalsol2005@gmail.com>
2022-11-25 16:49:59 +0200

    fixed client handshake induction

--- ClientForm/MainView.cs
+++ ClientForm/MainView.cs
@@ -43,7 +43,9 @@ namespace ClientForm
                 PacketManager.BuildIpv4Layer(),
                 PacketManager.BuildUdpLayer(myPort, PacketManager.SERVER_PORT));
 
-            Packet handshake_packet = handshake.Induction("127.0.0.1", myPort, 0, 0, true, 0); // *** need to change peer id***
+            DateTime now = DateTime.Now;
+
+            Packet handshake_packet = handshake.Induction(SRTManager.ProtocolManager.GenerateCookie("127.0.0.1", myPort, now), 0, 0, true, 0); // *** need to change peer id***
 
             /*Packet packet = new PacketBuilder(PacketManager.BuildEthernetLayer(),
                 PacketManager.BuildIpv4Layer(),
