5b5841ebe1904642adda3ee885b5b9f73ad52b87
EyalSol <eyalsol2005@gmail.com>
2022-11-27 18:02:42 +0200

    .

--- PcapFunc/ProtocolFields/Header.cs
+++ PcapFunc/ProtocolFields/Header.cs
@@ -11,13 +11,12 @@ namespace SRTManager.ProtocolFields
 
         public SRTHeader(SRTManager.ProtocolFields.PacketType packet_type, uint dest_socket_id, uint type_specific_info = 0)
         {
-            byteFields.Add(BitConverter.GetBytes(1)); // change to 1 bit 
+            //byteFields.Add(BitConverter.GetBytes(1)); // change to 1 bit 
             CONTROL_TYPE = (ushort)packet_type; byteFields.Add(BitConverter.GetBytes(CONTROL_TYPE)); // transfer to 15 bits (instead of 16)
 
             SUB_TYPE = 0x0; byteFields.Add(BitConverter.GetBytes(SUB_TYPE));
             TYPE_SPECIFIC_INFO = type_specific_info; byteFields.Add(BitConverter.GetBytes(TYPE_SPECIFIC_INFO));
             DEST_SOCKET_ID = dest_socket_id; byteFields.Add(BitConverter.GetBytes(DEST_SOCKET_ID));
-        }
 
 
         /// <summary>
