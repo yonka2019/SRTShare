f3ddb65621030b5d579aa2e171885c2811d7d761
Yonka <yonka2003@gmail.com>
2022-12-10 21:31:26 +0200

    ka manager done

--- Server/KeepAliveManager.cs
+++ Server/KeepAliveManager.cs
@@ -1,6 +1,7 @@
 ﻿using PcapDotNet.Packets;
 using SRTManager;
 using System.Threading;
+using System.Timers;
 
 using SRTRequest = SRTManager.RequestsFactory;
 
@@ -11,6 +12,9 @@ namespace Server
         private readonly uint socket_id;
         private readonly ushort socket_port;
         private int timeoutSeconds;
+        private bool connected;
+
+        private static System.Timers.Timer timer;
 
         public delegate void Notify(uint socket_id);
         public event Notify LostConnection;
@@ -21,6 +25,24 @@ namespace Server
             this.socket_id = socket_id;
             this.socket_port = socket_port;
             timeoutSeconds = 0;
+            connected = true;
+
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            timeoutSeconds++;
+
+            if (timeoutSeconds == 5)  // KEEP-ALIVE TIMED-OUT
+            {
+                LostConnection.Invoke(socket_id);
+                connected = false;
+
+                timer.Stop();
+                timer.Dispose();
+            }
         }
 
         public void StartCheck()
@@ -30,30 +52,27 @@ namespace Server
             kaChecker.Start(socket_id);
         }
 
-        public void ConfirmStatus()
+        public void ConfirmStatus()  // reset timeout seconds
         {
-            SetTimeoutTime(0); // add stopwatch and etc
-        }
-
-        private void SetTimeoutTime(int seconds)
-        {
-            timeoutSeconds = seconds;
+            timeoutSeconds = 0;
         }
 
         private void KeepAliveChecker(object dest_socket_id)
         {
             uint u_dest_socket_id = (uint)dest_socket_id;
+            timer.Start();
 
-            while (true)
+            while (connected)
             {
                 SRTRequest.KeepAliveRequest keepAlive_request = new SRTRequest.KeepAliveRequest
                                 (PacketManager.BuildBaseLayers(PacketManager.SERVER_PORT, socket_port));
 
                 Packet keepAlive_packet = keepAlive_request.Check(u_dest_socket_id);
                 PacketManager.SendPacket(keepAlive_packet);
+
+                Thread.Sleep(1000);  // 1 second wait
             }
         }
     }
-
 }
 
