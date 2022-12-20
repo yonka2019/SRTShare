1e89a71f11e1ce579ac6840d458efed0d399b908
Yonka <yonka2003@gmail.com>
2022-12-20 13:38:18 +0200

    data added

---
+++ SRTLibrary/SRTManager/ProtocolFields/Data/DataFlags.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRTManager.ProtocolFields.Data
+{
+    public enum PositionFlags : ushort
+    {
+        FIRST = 0x010b,
+        MIDDLE = 0x000b,
+        LAST = 0x001b,
+        SINGLE_DATA_PACKET = 0x011b
+    }
+
+    public enum EncryptionFlags : ushort
+    {
+        NOT_ENCRYPTED = 0x000b,
+        EVEN_KEY = 0x001b,
+        ODD_KEY = 0x010b,
+    }
+}
