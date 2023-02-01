2d05c28202f4a5f7224472888222efeaf25b172b
EyalSol <eyalsol2005@gmail.com>
2023-02-01 18:42:06 +0200

    changed to substitution algo to debug

--- SRTShareLib/SRTManager/Encryption/AES128.cs
+++ SRTShareLib/SRTManager/Encryption/AES128.cs
@@ -12,48 +12,89 @@ namespace SRTShareLib.SRTManager
         /// </summary>
         internal static EncryptionType Type => EncryptionType.AES128;
 
+        static byte[] substitutionTable = subTable();
+
+        public static byte[] subTable()
+        {
+            byte[] table = new byte[256];
+            for (int i = 0; i < 256; i++)
+            {
+                table[i] = (byte)((i + 128) % 256);
+            }
+
+            return table;
+        }
+
         internal static byte[] Encrypt(byte[] data, byte[] Key, byte[] IV)
         {
-            byte[] encrypted;
-
-            using (AesManaged aes = new AesManaged())
+            byte[] encrypted = new byte[data.Length];
+            for (int i = 0; i < data.Length; i++)
             {
-                aes.Padding = PaddingMode.Zeros;
-
-                ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
-
-                using (MemoryStream ms = new MemoryStream())
-                {   
-                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-                    {
-                        cs.Write(data, 0, data.Length);
-                    }
-                    encrypted = ms.ToArray();
-                }
+                encrypted[i] = substitutionTable[data[i]];
             }
             return encrypted;
         }
 
         internal static byte[] Decrypt(byte[] data, byte[] Key, byte[] IV)
         {
-            byte[] decrypted;
-
-            using (AesManaged aes = new AesManaged())
+            byte[] decrypted = new byte[data.Length];
+            for (int i = 0; i < data.Length; i++)
             {
-                aes.Padding = PaddingMode.Zeros;
-
-                ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
-
-                using (MemoryStream ms = new MemoryStream(data))
+                for (int j = 0; j < substitutionTable.Length; j++)
                 {
-                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
+                    if (data[i] == substitutionTable[j])
                     {
-                        cs.Write(data, 0, data.Length);
+                        decrypted[i] = (byte)j;
+                        break;
                     }
-                    decrypted = ms.ToArray();
                 }
             }
             return decrypted;
         }
+
+
+        //internal static byte[] Encrypt(byte[] data, byte[] Key, byte[] IV)
+        //{
+        //    byte[] encrypted;
+
+        //    using (AesManaged aes = new AesManaged())
+        //    {
+        //        aes.Padding = PaddingMode.Zeros;
+
+        //        ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
+
+        //        using (MemoryStream ms = new MemoryStream())
+        //        {   
+        //            using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+        //            {
+        //                cs.Write(data, 0, data.Length);
+        //            }
+        //            encrypted = ms.ToArray();
+        //        }
+        //    }
+        //    return encrypted;
+        //}
+
+        //internal static byte[] Decrypt(byte[] data, byte[] Key, byte[] IV)
+        //{
+        //    byte[] decrypted;
+
+        //    using (AesManaged aes = new AesManaged())
+        //    {
+        //        aes.Padding = PaddingMode.Zeros;
+
+        //        ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
+
+        //        using (MemoryStream ms = new MemoryStream(data))
+        //        {
+        //            using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
+        //            {
+        //                cs.Write(data, 0, data.Length);
+        //            }
+        //            decrypted = ms.ToArray();
+        //        }
+        //    }
+        //    return decrypted;
+        //}
     }
 }
