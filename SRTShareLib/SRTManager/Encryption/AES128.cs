e2f871987bc886ad3bf5b50d18d49f3bbcb365c7
Yonka <yonka2003@gmail.com>
2023-02-02 00:57:48 +0200

    huge comment added =D

--- SRTShareLib/SRTManager/Encryption/AES128.cs
+++ SRTShareLib/SRTManager/Encryption/AES128.cs
@@ -4,6 +4,18 @@ using System.Text;
 
 namespace SRTShareLib.SRTManager.Encryption
 {
+    /*
+     * This encryption method is problematic because this encryption use "hash" function in order to encrypt/decrypt the data.
+     * Which means, that even if one bit will lost from the packet or will received in the wrong way, the whole packet will be damaged and unsuitable to decrypt.
+     * In other words, this encrypytion method require the packet to be received fully as it was sent, and because the protocol which uses UDP connection as base,
+     * the packets come damaged, and can't be decrypted and read correctly, which causes a lot of interference and unreadable screen-share.
+     * There is two ways to cope with this issue, the first one is to add support of retransmission, to ensure that the packets would be received correctly.
+     * Another way, is to use encryption which operates on each byte individually and doesn't depend on any other bytes in the list, with this way,
+     * even if any bit lost, the packet will be avaible to decryption.
+     * 
+     * AES128 - depend on the other bytes because the hash encoding which is problematically in UDP based protocol (although, retransmission could help)
+     * Substitution / XOR - doesn't depend on other bytes, so they have better performance
+     */
     public static class AES128
     {
         /// <summary>
