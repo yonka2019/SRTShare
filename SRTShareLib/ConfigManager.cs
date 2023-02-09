d2722847d4fe129ebb8a19eec9db33b3505d505b
Yonka <yonka2003@gmail.com>
2023-02-09 17:46:44 +0200

    text changed

--- SRTShareLib/ConfigManager.cs
+++ SRTShareLib/ConfigManager.cs
@@ -92,12 +92,12 @@ namespace SRTShareLib
             while (!ipAddress)
             {
                 if (calledFrom == App.Server)
-                    Console.WriteLine("* Put here your own LOCAL IP (LAN). Even if you are using external connection\n[OR] you can input \"my\" to auto-set your local ip.\n");
+                    Console.WriteLine("* Put here your own LOCAL IP (LAN). Even if you are using external connection.\n[OR] you can input \"my\" to auto-set your local ip.\n");
 
                 else if (calledFrom == App.Client)
                     Console.WriteLine("* If your server in the same subnet with the client\n" +
                     "put here the local ip of the server (LAN), either, put the public one (WAN).\n" +
-                    "In addition, you can also put here a hostname (DNS Supported)");
+                    "In addition, you can also put here a hostname (DNS Supported).");
 
                 Console.Write(">> Server IP: ");
                 IP = Console.ReadLine();
