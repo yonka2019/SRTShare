069346ce167f1157637305f35a5f76ed2a405b48
Yonka <yonka2003@gmail.com>
2023-02-16 13:39:38 +0200

    up

--- Client/ImageDisplay.cs
+++ Client/ImageDisplay.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-
+using System.Linq;
 using Data = SRTShareLib.SRTManager.ProtocolFields.Data;
 
 namespace Client
@@ -9,8 +10,24 @@ namespace Client
     internal class ImageDisplay
     {
         private static ushort lastDataPosition;
-        private static readonly List<byte> allChunks = new List<byte>();
         private static readonly object _lock = new object();
+
+        private static List<Data.SRTHeader> dataPackets = new List<Data.SRTHeader>();
+
+        private static byte[] FullData
+        {
+            get
+            {
+                List<byte> fullData = new List<byte>();
+
+                foreach (Data.SRTHeader srtHeader in dataPackets)  // add each data into [List<byte> fullData]
+                {
+                    fullData.AddRange(srtHeader.DATA);
+                }
+                // convert to byte array and return
+                return fullData.ToArray();
+            }
+        }
 
         internal static void ProduceImage(Data.SRTHeader data_request, Cyotek.Windows.Forms.ImageBox imageBoxDisplayIn)
         {
@@ -20,23 +37,23 @@ namespace Client
             {
                 if (data_request.PACKET_POSITION_FLAG == (ushort)Data.PositionFlags.FIRST)
                 {
-                    if (lastDataPosition == (ushort)Data.PositionFlags.MIDDLE)  // last lost, image received
+                    if (lastDataPosition == (ushort)Data.PositionFlags.MIDDLE)  // LAST lost, image received [FIRST MID MID MID ---- FIRST]
                     {
                         ShowImage(false, imageBoxDisplayIn);
-                        allChunks.Clear();
+                        dataPackets.Clear();
                     }
-                    allChunks.AddRange(data_request.DATA);
+                    dataPackets.Add(data_request);
                 }
 
                 else if (data_request.PACKET_POSITION_FLAG == (ushort)Data.PositionFlags.LAST)  // full image received (but maybe middle packets get lost)
                 {
-                    allChunks.AddRange(data_request.DATA);
+                    dataPackets.Add(data_request);
                     ShowImage(true, imageBoxDisplayIn);
-                    allChunks.Clear();
+                    dataPackets.Clear();
                 }
                 else
                 {
-                    allChunks.AddRange(data_request.DATA);
+                    dataPackets.Add(data_request);
                 }
 
                 lastDataPosition = data_request.PACKET_POSITION_FLAG;
@@ -50,7 +67,10 @@ namespace Client
                 Debug.WriteLine("[IMAGE] ERROR: LAST chunk missing (SHOWING IMAGE)\n");
 #endif
 
-            using (MemoryStream ms = new MemoryStream(allChunks.ToArray()))
+            var a = MissingPackets();
+            Console.WriteLine(string.Join(",", a));
+
+            using (MemoryStream ms = new MemoryStream(FullData))
             {
                 try
                 {
@@ -61,6 +81,26 @@ namespace Client
                     Debug.WriteLine("[IMAGE] ERROR: Can't build image\n");
                 }
             }
+        }
+
+        private static uint[] MissingPackets()
+        {
+            dataPackets = dataPackets.OrderBy(dp => dp.MESSAGE_NUMBER).ToList();
+
+            List<uint> missingList = new List<uint>();
+            for (int i = 0; i < dataPackets.Count - 1; i++)
+            {
+                uint diff = dataPackets[i + 1].MESSAGE_NUMBER - dataPackets[i].MESSAGE_NUMBER;
+                if (diff > 1)
+                {
+                    for (uint j = 1; j < diff; j++)
+                    {
+                        missingList.Add(dataPackets[i].MESSAGE_NUMBER + j);
+                    }
+                }
+            }
+
+            return missingList.ToArray();
         }
     }
 }
